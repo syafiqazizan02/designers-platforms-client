e0d43c23cb375ed7fa18f82de902160026fc2b39
Syafiq Azizan <syafiqaizan85@gmail.com>
2020-10-26 23:35:04 +0800

    verify.vue & resend.vue added

---
+++ pages/auth/verification/verify.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Verify Email </h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
--- router.js
+++ router.js
@@ -8,7 +8,9 @@ const routes = [
   { path: '/', name: 'index', component: page('index.vue') },
   { path: '/', name: 'about', component: page('about.vue') },
   { path: '/login', name: 'login', component: page('auth/login.vue') },
-  { path: '/register', name: 'register', component: page('auth/register.vue') }
+  { path: '/register', name: 'register', component: page('auth/register.vue') },
+  { path: '/verification/verify/:id', name: 'verify', component: page('auth/verification/verify.vue') },
+  { path: '/verification/resend', name: 'resend', component: page('auth/verification/resend.vue') }
 ];
 
 export function createRouter() {
