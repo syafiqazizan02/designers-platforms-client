8536da1026c2750a09d583025dafaedf1bab007e
Syafiq Azizan <syafiqaizan85@gmail.com>
2020-10-27 00:58:23 +0800

    setup dynamic components registration

---
+++ components/_global/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+import { __esModule } from 'vform';
+
+// require all components in the current directory and subdirectories (by webpack)
+const requireComponent = require.context('.', true, /_base-[\w-]+\.vue$/); // _filename base endup with .vue
+
+requireComponent.keys().forEach(fileName => {
+  // get the component's configuration
+  const componentConfig = requireComponent(fileName);
+
+  const fn = fileName
+    .split('/') // get filename path
+    .pop() // get the component by last
+    .replace(/\.\w+$/, ''); // remove .vue
+
+  const componentName = upperFirst(camelCase(fn));
+
+  Vue.component(componentName, componentConfig.default || componentConfig); // register components by default (by webpack)
+});
--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~plugins/vform'
+    '~plugins/vform',
+    '~components/_global' // register global component 
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
