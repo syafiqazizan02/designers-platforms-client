1812fd01222dfb5b39621660c979c83839b15aef
Syafiq Azizan <syafiqaizan85@gmail.com>
2020-10-27 16:54:39 +0800

    complate update image

--- router.js
+++ router.js
@@ -14,7 +14,8 @@ const routes = [
   { path: '/password/email', name: 'password.email', component: page('auth/password/reset-email.vue') },
   { path: '/password/reset/:token', name: 'password.reset', component: page('auth/password/password-reset.vue') },
   { path: '/user/dashboard', name: 'user.dashboard', component: page('user/dashboard.vue') },
-  { path: '/upload', name: 'designs.upload', component: page('user/designs/create.vue') }
+  { path: '/upload', name: 'designs.upload', component: page('user/designs/create.vue') },
+  { path: '/designs/:id/edit', name: 'designs.edit', component: page('user/designs/edit.vue') }
 ];
 
 export function createRouter() {
