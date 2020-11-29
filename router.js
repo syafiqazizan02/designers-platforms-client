78282344d1c3991778c916cdf0defdea7fa08023
SyafiqAz <developmentsyafiq@gmail.com>
2020-11-30 00:06:24 +0800

    apply design commments & likes

--- router.js
+++ router.js
@@ -40,7 +40,8 @@ const routes = [
       }	      
     ]
   },
-  { path: '/designs', name: 'designs.search', component: page('designs/search.vue')}
+  { path: '/designs', name: 'designs.search', component: page('designs/search.vue')},
+  { path: '/design/:slug', name: 'designs.show', component: page('designs/show.vue')}
 ];
 
 export function createRouter() {
